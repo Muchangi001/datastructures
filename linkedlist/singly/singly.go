d37393c1ed7359c65d16b1a1ffae2bc9a3b4d4b0
Muchangi001 <andrewmuchangi254@gmail.com>
2024-10-12 13:17:09 +0300

    minor changes

--- linkedlist/singly/singly.go
+++ linkedlist/singly/singly.go
@@ -15,33 +15,45 @@ type SinglyLinkedList struct {
 	tail *Node
 }
 
-func (s *SinglyLinkedList) init(h *Node, t *Node) {
+func (s *SinglyLinkedList) Init(h *Node, t *Node) {
 	if s.head != nil {
 		log.Println("Error : failed to initialize singly linked list, pointer to head is not null")
 		return
 	}
 	if s.tail != nil {
-		log.Println("Error : failed to initialize singly linked list, pointer to tail is not null")
+		log.Println("Error : failed to initialize singly linked list, pointer to tail is not nil")
 		return
 	}
 	s.head = h
 	s.tail = t
 }
 
-func (s *SinglyLinkedList) printList() {
+func (s *SinglyLinkedList) GetSize() (count uint) {
+	count = uint(0)
+	for currNode := s.head; currNode != nil; currNode = currNode.next {
+		count++
+	}
+	return
+}
+
+func (s *SinglyLinkedList) PrintList() {
 	if s.head == nil {
-		log.Println("Error : failed print singly linked list, pointer to head is null. Make sure to initialize the list first")
+		log.Println("Error : failed to print singly linked list, pointer to head is nil. Make sure to initialize the list first")
 		return
 	}
 
 	i := 0
-	for currentNode := s.head ; currentNode.next != nil; currentNode = currentNode.next {
-		fmt.Printf("Node [%v] -> %v", i, currentNode.data)
+	for currNode := s.head ; currNode != nil; currNode = currNode.next {
+		fmt.Printf("Node [%v] -> %v\n", i, currNode.data)
 		i++
 	}
 }
 
-func (s *SinglyLinkedList) prepend(n *Node) {
+func (s *SinglyLinkedList) Prepend(n *Node) {
+	if n == nil {
+		log.Println("Error : failed to prepend, pointer to node is nil")
+		return
+	}
 	if s.head == nil {
 		s.head = n
 		return
@@ -54,4 +66,79 @@ func (s *SinglyLinkedList) prepend(n *Node) {
 	}
 	n.next = s.head
 	s.head = n
+}
+
+func (s *SinglyLinkedList) Insert(n *Node, i uint) {
+	if n == nil {
+		log.Println("Error : failed to insert, pointer to node is nil")
+		return
+	}
+	if i == 0 {
+		s.Prepend(n)
+	}
+	if i > s.GetSize() {
+		log.Println("Error : failed to insert node, index is out of range")
+		return
+	}
+	currNode := s.head
+	for j := uint(0); j != i - 1; j++{
+		currNode = currNode.next
+	}
+	n.next = currNode.next
+	currNode.next = n
+}
+
+func (s *SinglyLinkedList) Append(n *Node) {
+	if s.head == nil {
+		s.head = n
+		s.tail = s.head
+		return
+	}
+	if s.head == s.tail {
+		s.tail = n
+		s.head.next = s.tail
+		return
+	}
+	s.tail.next = n
+	s.tail = n
+}
+
+func (s *SinglyLinkedList) DeleteHead() {
+	if s.head == nil {
+		log.Println("Error : failed to delete head, pointer to head is nil")
+		return
+	}
+	s.head = nil
+}
+
+func (s *SinglyLinkedList) DeleteNode(i uint) {
+	if i == 0 {
+		s.DeleteHead()
+	}
+	
+	currNode := s.head
+	for j := uint(0); j != i - 1; j++ {
+		currNode = currNode.next
+	}
+	currNode.next = currNode.next.next
+}
+
+func (s *SinglyLinkedList) DeleteTail() {
+	if s.tail == nil {
+		log.Println("Error : failed to delete tail, pointer to tail is nil")
+		return
+	}
+	currNode := s.head;
+	for ; currNode.next != s.tail; currNode = currNode.next {
+	}
+	currNode.next = nil
+	s.tail = currNode
+}
+
+func (s *SinglyLinkedList) GetNodePosition(n *Node) (i uint) {
+	i = uint(0)
+	for currNode := s.head; currNode != n; currNode = currNode.next {
+		i++
+	}
+	return 
 }
